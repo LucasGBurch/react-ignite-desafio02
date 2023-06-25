4bbbf53c879e39691de4d9782c24fc79715de3f7
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-06-25 20:42:53 -0300

    intro da primeira pagina pronta

--- src/pages/Home/components/Intro/index.tsx
+++ src/pages/Home/components/Intro/index.tsx
@@ -1,5 +1,44 @@
-import { IntroContainer } from "./styles";
+import { IntroContainer } from './styles';
+import cupIntroImg from './../../../../assets/IntroImg.svg';
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react';
+import { useTheme } from 'styled-components';
 
 export function Intro() {
-  return <IntroContainer></IntroContainer>
+  const theme = useTheme();
+
+  return (
+    <IntroContainer>
+      <div>
+        <h2>Encontre o café perfeito para qualquer hora do dia</h2>
+        <p>
+          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+          hora
+        </p>
+
+        <div>
+          <div>
+            <span>
+              <ShoppingCart id='ShopCartIntro' size={32} weight='fill' />
+              Compra simples e segura
+            </span>
+
+            <span>
+              <Timer id='TimerIntro' size={32} weight='fill' />
+              Entrega rápida e rastreada
+            </span>
+          </div>
+          <div>
+            <span>
+              <Package id='PackageIntro' size={32} weight='fill' />
+              Embalagem mantém o café intacto
+            </span>
+            <span>
+              <Coffee id='CoffeeIntro' size={32} weight='fill' />O café chega fresquinho até você
+            </span>
+          </div>
+        </div>
+      </div>
+      <img src={cupIntroImg} />
+    </IntroContainer>
+  );
 }
--- src/pages/Home/components/Intro/styles.ts
+++ src/pages/Home/components/Intro/styles.ts
@@ -2,4 +2,83 @@ import styled from 'styled-components';
 
 export const IntroContainer = styled.section`
   height: 34rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  gap: 3.5rem;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    height: 21.375rem;
+
+    padding-bottom: 1.125rem;
+    width: 36.75rem;
+
+    h2 {
+      color: ${(props) => props.theme['base-title']};
+      font-family: 'Baloo 2', cursive;
+      font-size: 3rem;
+      font-weight: 700;
+      line-height: 130%;
+      margin-bottom: 1rem;
+    }
+
+    p {
+      color: ${(props) => props.theme['base-subtitle']};
+      font-size: 1.5rem;
+      line-height: 130%;
+      width: 36.75rem;
+      margin-bottom: 4.125rem;
+    }
+
+    div {
+      height: 5.25rem;
+      width: 35.438rem;
+
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      gap: 1.25rem;
+
+      div {
+        width: 14.438rem;
+      }
+
+      div:last-child {
+        width: 18.375rem;
+      }
+
+      span {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        justify-content: center;
+        font-size: 1rem;
+
+        svg {
+          color: ${(props) => props.theme.white};
+          border-radius: 1000px;
+          padding: 0.5rem;
+        }
+      }
+
+      #ShopCartIntro {
+        background-color: ${(props) => props.theme['yellow-dark']};
+      }
+
+      #TimerIntro {
+        background-color: ${props => props.theme.yellow};
+      }
+
+      #PackageIntro {
+        background-color: ${props => props.theme['base-text']};
+      }
+
+      #CoffeeIntro {
+        background-color: ${props => props.theme.purple};
+      }
+    }
+  }
 `;
