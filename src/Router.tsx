855052b68ff82991427e1aff28b3ffede71630ba
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-07-31 18:09:41 -0300

    Removendo anotações de Icons

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,22 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { DefaultLayout } from './layouts/DefaultLayout';
 
-/*LISTA DE ÍCONES PHOSPHOR (fill é com propriedade "weight" e cor "color") */
-import {
-  MapPin, // fill
-  MapPinLine,
-  Trash,
-  CurrencyDollar,
-  ShoppingCart, // fill
-  Timer, // fill
-  Package, // fill
-  Coffee, // fill
-  Plus, // bold
-  Minus, // bold
-  CreditCard,
-  Money,
-  Bank,
-} from 'phosphor-react';
 import { Home } from './pages/Home';
 import { Checkout } from './pages/Checkout';
 import { Success } from './pages/Success';
